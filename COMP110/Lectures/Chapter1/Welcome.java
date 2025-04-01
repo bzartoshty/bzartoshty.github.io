37478de9aaef5cc27ec3b1d9ffa94dd297e6176e
bzartoshty <33467997+bzartoshty@users.noreply.github.com>
2025-04-01 14:30:32 -0700

    Add files via upload

--- COMP110/Lectures/Chapter1/Welcome.java
+++ COMP110/Lectures/Chapter1/Welcome.java
@@ -1,1 +1,16 @@
+/* File: Welcome.java
+   Name: Bahram Zartoshty
+   This program prints Welcome to Java!
+*/
+public class Welcome 
+{
+   public static void main(String[] args) 
+   {
+   
+      System.out.println("Welcome to java!");
+   
+   
+   }
+
+}
 
