0171a36f77c36ca283640c7bd7cccab150130d1f
bzartoshty <33467997+bzartoshty@users.noreply.github.com>
2025-04-02 15:46:49 -0700

    Add files via upload

--- COMP182/Lectures/Lecture1/TwoDimensionalArrayExamples.java
+++ COMP182/Lectures/Lecture1/TwoDimensionalArrayExamples.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class TwoDimentionalArrayExamples {
+public class TwoDimensionalArrayExamples {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
