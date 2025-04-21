c9b014d4ba5152bdd8ae5ff6ad64714f844b0aae
bzartoshty <33467997+bzartoshty@users.noreply.github.com>
2025-04-21 14:57:34 -0700

    Add files via upload

--- COMP110/Lectures/Chapter9/TotalArea.java
+++ COMP110/Lectures/Chapter9/TotalArea.java
@@ -5,6 +5,7 @@ public class TotalArea {
       Circle[] arrayCircle = new Circle[5];
       for(int i=0; i<5; ++i){
          arrayCircle[i] = new Circle(rand.nextInt(20)+1);
+         System.out.println("Circle with radius  "+arrayCircle[i].getRadius()+" has area of "+String.format("%.2f",arrayCircle[i].getArea()));
       }
       System.out.println("Total area is: "+sum(arrayCircle));
    }
