c5be40109ac2cf186da580e3bf0b457ef680a2d5
Bahram Zarttoshty <33467997+bzartoshty@users.noreply.github.com>
2026-04-06 12:53:51 -0700

    Add files via upload

--- COMP182/Lectures/Lecture10/TestBST.java
+++ COMP182/Lectures/Lecture10/TestBST.java
@@ -1,41 +1,22 @@
-import java.util.*;
-
-
 public class TestBST {
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        BST<Integer> tree = new BST<>();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(80);
 
-	
-		// TODO Auto-generated method stub
-		public static void main(String[] args) {
-		    new TestBST();
-		  }
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(65);
+        tree.insert(90);
+        tree.insert(60);
+       
 
-		  public TestBST() {
-           
-			 BST<Integer> tree = new BST<>();   
-			  tree.insert(50);
-			  tree.insert(30);
-			  tree.insert(80);
-			  
-			  tree.insert(20);
-			  tree.insert(40);
-			  tree.insert(65);
-			  tree.insert(90);
-			  tree.insert(60);
-			  tree.remove(50);
-
-			  for(Integer e : tree) {
-				  System.out.println(e);
-			  }
-			   
-			  
-			 
-			    
-			   
-			   
-		  }
-		  
-		
-
-	}
+        for(Integer e : tree) {
+            System.out.println(e);
+        }
 
 
+    }
+}
