163571cc77c8f39c9cf803b465374b00e9c557b2
Bahram Zarttoshty <33467997+bzartoshty@users.noreply.github.com>
2026-07-13 14:49:45 -0700

    Add files via upload

--- COMP282/Lectures/Lecture02/BinarySearchTree.java
+++ COMP282/Lectures/Lecture02/BinarySearchTree.java
@@ -1,29 +1,56 @@
 import java.util.*;
 
-import java.io.*;
-
-public class        BinarySearchTree<E extends Comparable<E>> implements Tree<E>{
+public class BinarySearchTree<E extends Comparable<E>> implements Iterable<E> {
     protected TreeNode<E> root;
     protected int size;
 
 
 
-    /** Create a default BST2  */
+    /** Create a default BinarySearchTree  */
     public BinarySearchTree() {
         root = null;
         size = 0;
     }
 
+    public boolean insertRecrusive(E e) {
+        if(root == null) {
+            root = createNewNode(e);
+            size++;
+            return true;
+        }
+        else
+            return insertRecursive(e, root);
+    }
 
+    private boolean insertRecursive(E e, TreeNode<E> curr) {
+        if(e.compareTo(curr.element)<0) {
+            if(curr.left == null) {
+                curr.left = createNewNode(e);
+                size++;
+                return true;
+            }
+            return insertRecursive(e,curr.left);
+        }
+        else if(e.compareTo(curr.element)>0) {
+            if(curr.right == null) {
+                curr.right = createNewNode(e);
+                size++;
+                return true;
+            }
+            else
+                return insertRecursive(e,curr.right);
+        }
+        else
+            return true;
+    }
 
     /** Create a binary tree from an array of objects */
     public BinarySearchTree(E[] objects) {
 
         for (int i = 0; i < objects.length; i++)
-            add(objects[i]);
+            insert(objects[i]);
     }
 
-    @Override /** Returns true if the element is in the tree */
     public boolean search(E e) {
         TreeNode<E> current = root; // Start from the root
 
@@ -41,8 +68,22 @@ public class        BinarySearchTree<E extends Comparable<E>> implements Tree<E>
         return false;
     }
 
-    /** Insert element e into the binary tree
-     * Return true if the element is inserted successfully */
+    public boolean find(E e) {
+        return find(e,root);
+    }
+
+    protected boolean find(E e, TreeNode<E> current) {
+        if(current==null)
+            return false;
+        else if(current.element.equals(e))
+            return true;
+        else if(e.compareTo(current.element)<0)
+            return find(e,current.left);
+        else
+            return find(e,current.right);
+
+    }
+
     public boolean insert(E e) {
         if (root == null)
             root = createNewNode(e); // Create a new root
@@ -73,46 +114,13 @@ public class        BinarySearchTree<E extends Comparable<E>> implements Tree<E>
         return true; // Element inserted successfully
     }
 
-    public boolean insertRecursive(E e) {
-        if (root == null) {
-            root = new TreeNode<>(e); // Create a new root
-            size++;
-            return true;
-        }
-        return insertRecursive(root,e);
-    }
-
-    public boolean insertRecursive(TreeNode<E> current, E e) {
-        if(e.compareTo(current.element)<0) {
-            if(current.left==null) {
-                current.left = new TreeNode<>(e);
-                size++;
-                return true;
-            }
-            else
-                return insertRecursive(current.left,e);
-        }
-        else if(e.compareTo(current.element)>0) {
-            if(current.right==null) {
-                current.right = new TreeNode<>(e);
-                size++;
-                return true;
-            }
-            else
-                return insertRecursive(current.right,e);
-        }
-        else
-            return true; // Element inserted successfully
-    }
-
-
     protected TreeNode<E> createNewNode(E e) {
         return new TreeNode<>(e);
     }
 
     /** Return the height of this binary tree */
     public int height() {
-        // Left as exercise
+
         return height(root);
 
     }
@@ -128,149 +136,36 @@ public class        BinarySearchTree<E extends Comparable<E>> implements Tree<E>
     }
 
     public int getHeight(TreeNode<E> root) {
-        Queue<TreeNode<E>> q = new LinkedList<>();
-        int height = 0;
-        // add root to the queue
-        q.add(root);
-        // add null as marker
-        q.add(null);
-        while (q.isEmpty() == false) {
-            TreeNode<E> n = q.remove();
-            // check if n is null, if yes, we have reached to the end of the
-            // current level, increment the height by 1, and add the another
-            // null as marker for next level
-            if (n == null) {
-                // before adding null, check if queue is empty, which means we
-                // have traveled all the levels
-                if(!q.isEmpty()){
-                    q.add(null);
-                }
-                height++;
-            }else{
-                // else add the children of extracted node.
-                if (n.left != null) {
-                    q.add(n.left);
-                }
-                if (n.right != null) {
-                    q.add(n.right);
-                }
-            }
-        }
+        // left as ecercise
+        int height=0;
         return height;
     }
-
-    /** Returns the first (lowest) element currently in this BST. */
-    public E first() {
-        if (root == null)
-            return null;
-        TreeNode<E> current = root;
-        while (current.left != null) {
-            current = current.left;
-        }
-        return current.element;
-    }
-
-    /** Returns the last (highest) element currently in this BST. */
-    public E last() {
-        if (root == null)
-            return null;
-        TreeNode<E> current = root;
-        while (current.right != null) {
-            current = current.right;
-        }
-        return current.element;
-    }
-
-    /** Returns the least element in this set greater than to the given element,
-     *   or null if there is no such element.
-     */
-    public E ceiling(E e) {
-        TreeNode<E> parent = findNode(e);
-        if(parent==null) {
-            System.out.println(e + " is not in the tree");
-            return null;
-        }
-        if (parent.right == null)
-            return null;
-        TreeNode<E> node = parent.right;
-        if(node.left == null){
-            return node.element;
-        }
-        while (node.left != null) {
-            node = node.left;
-        }
-        return node.element;
-    }
-
-    private TreeNode<E> findNode(E e) {
-        TreeNode<E> current = root;
-        while (current != null) {
-            if (e.compareTo(current.element) < 0) {
-                current = current.left;
-            }
-            else if (e.compareTo(current.element) > 0) {
-                current = current.right;
-            }
-            else if(e.compareTo(current.element) == 0){
-                return current;
-            }
-        }
-        return null;
-    }
-
-    /** Returns the greatest element in this set less than to the given element,
-     * or null if there is no such element.
-     */
-   public E floor(E e) {
-       TreeNode<E> parent = findNode(e);
-       if(parent==null) {
-           System.out.println(e + " is not in the tree");
-           return null;
-       }
-       if (parent.left == null)
-           return null;
-       TreeNode<E> node = parent.left;
-       if(node.right == null){
-           return node.element;
-       }
-       while (node.right != null) {
-           node = node.right;
-       }
-       return node.element;
-   }
-
-
-
 
     /** BreadthFirstSearch traversal from the root */
     public  void breadthFirstTraversal() {
         // Left as an exercise
-        java.util.Queue<TreeNode<E>> queue =
-                new java.util.LinkedList<TreeNode<E>>();
-
-
-        if (root == null)
-            return ;
-
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            TreeNode<E> node = queue.poll();
-
-            System.out.print(node.element + " ");
-            if (node.left != null)
-                queue.add(node.left);
-            if (node.right != null)
-                queue.add(node.right);
+        Queue<TreeNode<E>> q = new LinkedList<>();
+        LinkedList<TreeNode<E>> list = new LinkedList<>();
+        if(root == null)
+            return;
+        q.add(root);
+        while(!q.isEmpty()) {
+            TreeNode<E> node = q.remove();
+            list.add(node);
+            if(node.left != null)
+                q.add(node.left);
+            if(node.right != null)
+                q.add(node.right);
         }
-
-
+        System.out.println(list);
 
     }
 
 
 
-    @Override /** Inorder traversal from the root */
+
+
+
     public void inorder() {
         inorder(root);
     }
@@ -301,13 +196,13 @@ public class        BinarySearchTree<E extends Comparable<E>> implements Tree<E>
                     s.push(node.right);
             }
         }
+        // for(int i=0;i<list.size();++i)
+        //	  System.out.print(list.get(i).element+" ");
         System.out.println(list);
     }
 
 
 
-
-    @Override /** Postorder traversal from the root */
     public void postorder() {
         postorder(root);
     }
@@ -339,11 +234,15 @@ public class        BinarySearchTree<E extends Comparable<E>> implements Tree<E>
                 list.add(s.pop());
             }
         }
+	 /* for(int i=0;i<list.size();++i) {
+		  System.out.print(list.get(i).element+" ");
+	  }
+	  */
         System.out.println(list);
     }
 
 
-    @Override /** Preorder traversal from the root */
+
     public void preorder() {
         preorder(root);
     }
@@ -373,7 +272,6 @@ public class        BinarySearchTree<E extends Comparable<E>> implements Tree<E>
                 stack.push(node.left);
         }
         System.out.println(list);
-        return;
 
     }
 
@@ -381,29 +279,13 @@ public class        BinarySearchTree<E extends Comparable<E>> implements Tree<E>
 
 
 
-
-
-    @Override /** Get the number of nodes in the tree */
     public int getSize() {
         return size;
-    }
-
-    public int getNumberOfNodes() {
-        return getNumberOfNodes(root);
-    }
-
-    public int getNumberOfNodes(TreeNode<E> root) {
-        if (root == null) return 0;
-        return 1 + getNumberOfNodes(root.left) + getNumberOfNodes(root.right);
     }
 
     /** Returns the root of the tree */
     public TreeNode<E> getRoot() {
         return root;
-    }
-
-    public E getElement(TreeNode<E> node) {
-        return node.element;
     }
 
     /** Returns a path from the root leading to the specified element */
@@ -426,10 +308,6 @@ public class        BinarySearchTree<E extends Comparable<E>> implements Tree<E>
 
         return list; // Return an array list of nodes
     }
-
-    @Override /** Delete an element from the binary tree.
-     * Return true if the element is deleted successfully
-     * Return false if the element is not in the tree */
 
     public boolean delete(E e) {
         // Locate the node to be deleted and also locate its parent node
@@ -486,89 +364,43 @@ public class        BinarySearchTree<E extends Comparable<E>> implements Tree<E>
                 // Special case: parentOfRightMost == current
                 parentOfRightMost.left = rightMost.left;
         }
-	    /*
-	    	 // Case 2: The current node has a left child
-	        // Locate the leftmost node in the right subtree of
-	        // the current node and also its parent
-	        TreeNode<E> parentOfLeftMost = current;
-	        TreeNode<E> leftMost = current.right;
+    /*
+    	 // Case 2: The current node has a left child
+        // Locate the leftmost node in the right subtree of
+        // the current node and also its parent
+        TreeNode<E> parentOfLeftMost = current;
+        TreeNode<E> leftMost = current.right;
 
-	        while (leftMost.left != null) {
-	          parentOfLeftMost = leftMost;
-	          leftMost = leftMost.left; // Keep going to the left
-	        }
+        while (leftMost.left != null) {
+          parentOfLeftMost = leftMost;
+          leftMost = leftMost.left; // Keep going to the left
+        }
 
-	        // Replace the element in current by the element in leftMost
-	        current.element = leftMost.element;
+        // Replace the element in current by the element in leftMost
+        current.element = leftMost.element;
 
-	        // Eliminate rightmost node
-	        if (parentOfLeftMost.left == leftMost)
-	          parentOfLeftMost.left = leftMost.right;
-	        else
-	          // Special case: parentOfRightMost == current
-	          parentOfLeftMost.right = leftMost.right;
-	      }
-	*/
+        // Eliminate rightmost node
+        if (parentOfLeftMost.left == leftMost)
+          parentOfLeftMost.left = leftMost.right;
+        else
+          // Special case: parentOfRightMost == current
+          parentOfLeftMost.right = leftMost.right;
+      }
+*/
         size--;
         return true; // Element deleted successfully
     }
 
 
-    @Override /** Remove all elements from the tree */
+
     public void clear() {
         root = null;
         size = 0;
     }
 
-    public void saveTree(String filename) throws IOException {
-        // Left as Exercise
-        ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(filename));
-        saveTree(root,output);
-        output.writeObject(null); // to indicate end of tree
-        output.close();
-    }
-
-    private void saveTree(TreeNode<E> root, ObjectOutputStream output) throws IOException {
-        if(root!=null) {
-
-            saveTree(root.left, output);
-            output.writeObject(root.element);
-            saveTree(root.right, output);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public void loadTree(String filename) throws IOException, ClassNotFoundException {
-        // Left as Exercise
-        ObjectInputStream input = new ObjectInputStream(new FileInputStream(filename));
-        ArrayList<E> list = new ArrayList<>();
-        while (true) {
-            E element = (E) input.readObject();
-            if (element == null)
-                break;
-            list.add(element);
-        }
-        input.close();
-        if (list.size() > 0) {
-            root = buildTree(list, 0, list.size() - 1);
-            size = list.size();
-        }
-    }
-
-    private TreeNode<E> buildTree(ArrayList<E> list, int first, int last) {
-        if (first > last)
-            return null;
-        int mid = (first + last) / 2;
-        TreeNode<E> left = buildTree(list, first, mid - 1);
-        TreeNode<E> node = new TreeNode<>(list.get(mid));
-        node.left = left;
-        node.right = buildTree(list, mid + 1, last);
-        return node;
-    }
 
 
 
-    @Override /** Obtain an iterator. Use inorder. */
     public java.util.Iterator<E> iterator() {
         return new InorderIterator();
     }
@@ -620,99 +452,27 @@ public class        BinarySearchTree<E extends Comparable<E>> implements Tree<E>
         }
     }
 
-    @SuppressWarnings("unchecked")
-    @Override
-    /** Returns true if this collection contains all of the elements in the specified collection. */
-    public  boolean containsAll(Collection<?> c) {
-        for (Object e : c)
-            if (!contains(e))
-                return false;
-        return true;
-    }
-
-    /** Adds all of the elements in the specified collection to this tree. */
-    public  boolean addAll(Collection<? extends E> c) {
-        for (E e : c)
-            add(e);
-        return true;
-    }
 
 
-    /** Remove all of the elements in the specified collection from this tree. */
-    public boolean removeAll(Collection<?> c) {
-        for (Object e : c)
-            remove(e);
-        return true;
-    }
 
-    /** Retain all of the elements in the specified collection in this tree. */
-    public boolean retainAll(Collection<?> c) {
-        for (Object e : this)
-            if (!c.contains(e))
-                remove(e);
-        return true;
-    }
+    /** This inner class is static, because it does not access
+     any instance members defined in its outer class */
+    private static class TreeNode<E>  {
+        protected E element;
+        protected TreeNode<E> left;
+        protected TreeNode<E> right;
 
-    /** Returns true if this tree contains the specified element. */
-    public boolean contains(Object e) {
-        return search((E)e);
-    }
-
-    /** Returns an array containing all of the elements in this collection.
-     * this method must return the elements in the same order level-be-level
-     */
-    public  Object[] toArray() {
-        Object[] array = new Object[size];
-        ArrayList<E> list = bfs();
-        for (int i = 0; i < list.size(); i++)
-            array[i] = list.get(i);
-        return array;
-    }
-
-    /** Returns an array containing all of the elements in this collection.
-     * this method must return the elements in the same order level-be-level
-     */
-    public E[] toArray(E[] array){
-        ArrayList<E> list = bfs();
-        for (int i = 0; i < list.size(); i++)
-            array[i] = list.get(i);
-        return array;
-    }
-
-    private  ArrayList<E> bfs() {
-        // Left as an exercise
-        java.util.Queue<TreeNode<E>> queue =
-                new java.util.LinkedList<TreeNode<E>>();
-        ArrayList<E> list = new ArrayList<>();
-
-        if (root == null)
-            return null;
-
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            TreeNode<E> node = queue.poll();
-            list.add(node.element);
-            if (node.left != null)
-                queue.add(node.left);
-            if (node.right != null)
-                queue.add(node.right);
+        public TreeNode(E e) {
+            element = e;
         }
 
-        return list;
-
+        public String toString() {
+            return element.toString();
+        }
     }
 
-    // Make a deep copy of this BST
-   public BinarySearchTree<E> clone() {
 
-       BinarySearchTree<E> clone = new BinarySearchTree<>();
-       clone.addAll(this);
-       return clone;
 
-   }
-    
-    
 
 
 }
